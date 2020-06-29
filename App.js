6a7c78df3359dd326aae8a23016f688d4db0643a
The-manpreet-singn <manpreet.singh10196@gmail.com>
2020-06-29 11:12:36 +0530

    add navigator in app

--- App.js
+++ App.js
@@ -1,13 +1,108 @@
-import React from "react";
-import { View } from "react-native";
-import DeckList from "./components/DeckList";
-import DeckListItem from "./components/DeckListItem";
+import "react-native-gesture-handler";
+import React, { useEffect } from "react";
+import { Platform, StyleSheet } from "react-native";
+import { white, purple, gray } from "./utils/colors";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { Icon } from "react-native-elements";
 
-export default function App() {
+import DeckList from "./components/DeckList";
+
+import { setLocalNotification, clearLocalNotification } from "./utils/notification";
+
+function Home() {
+	const Tab = createBottomTabNavigator();
 	return (
-		<View>
-			<DeckList />
-			{/* <DeckListItem /> */}
-		</View>
+		<Tab.Navigator>
+			<Tab.Screen
+				name="DeckList"
+				component={DeckListView}
+				options={{
+					tabBarLabel: "Deck List",
+					tabBarIcon: ({ tintColor }) => <Icon name="list" size={30} color={tintColor} />,
+				}}
+			/>
+			<Tab.Screen
+				name="NewDeck"
+				component={NewDeckView}
+				options={{
+					tabBarLabel: "New Deck",
+					title: "New Deck",
+					tabBarIcon: ({ tintColor }) => <Icon name="add" size={30} color={tintColor} />,
+				}}
+			/>
+		</Tab.Navigator>
 	);
 }
+
+export default function App() {
+	useEffect(() => {
+		setLocalNotification();
+	}, []);
+
+	const Stack = createStackNavigator();
+
+	return (
+		<NavigationContainer>
+			<Stack.Navigator>
+				<Stack.Screen name="Home" component={Home} options={{ title: "Deck List" }} />
+			</Stack.Navigator>
+			<Stack.Screen name="DeckList" component={DeckList} options={({ route }) => ({ title: route.params.title })} />
+		</NavigationContainer>
+	);
+}
+
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		alignItems: "center",
+	},
+	input: {
+		height: 40,
+		borderColor: gray,
+		borderWidth: 1,
+		marginBottom: 20,
+		paddingLeft: 3,
+		width: 300,
+		marginTop: 50,
+	},
+	deck: {
+		width: 250,
+		justifyContent: "space-around",
+		alignContent: "center",
+		backgroundColor: white,
+		borderColor: "black",
+		borderWidth: 1,
+		borderRadius: 5,
+		padding: 15,
+		paddingLeft: 25,
+		paddingRight: 25,
+		marginBottom: 20,
+		marginTop: 20,
+	},
+	deckTitle: {
+		fontSize: 20,
+		fontWeight: "bold",
+	},
+	deckSubTitle: {
+		fontSize: 13,
+		fontWeight: "bold",
+		paddingTop: 5,
+	},
+
+	button: {
+		width: 250,
+		flexDirection: "row",
+		justifyContent: "space-around",
+		alignContent: "center",
+		backgroundColor: white,
+		borderColor: purple,
+		borderWidth: 1,
+		borderRadius: 10,
+		padding: 15,
+		paddingLeft: 25,
+		paddingRight: 25,
+		marginBottom: 25,
+	},
+});
