a7ee6812ab8c7b798751b304ab83c8c3fe3ce405
The-manpreet-singn <manpreet.singh10196@gmail.com>
2020-06-29 10:21:59 +0530

    add DeckList in app.js

--- App.js
+++ App.js
@@ -1,19 +1,11 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { View } from "react-native";
+import DeckList from "./components/DeckList";
 
 export default function App() {
 	return (
-		<View style={styles.container}>
-			<Text>Open up App.js to start working on your app!</Text>
+		<View>
+			<DeckList/>
 		</View>
 	);
 }
-
-const styles = StyleSheet.create({
-	container: {
-		flex: 1,
-		backgroundColor: "#fff",
-		alignItems: "center",
-		justifyContent: "center",
-	},
-});
