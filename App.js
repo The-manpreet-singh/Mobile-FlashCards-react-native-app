b637debf0b870b495bd25e562652110622e9cf5e
The-manpreet-singn <manpreet.singh10196@gmail.com>
2020-06-29 11:04:21 +0530

    add hooks method

--- App.js
+++ App.js
@@ -7,7 +7,7 @@ export default function App() {
 	return (
 		<View>
 			<DeckList />
-			<DeckListItem />
+			{/* <DeckListItem /> */}
 		</View>
 	);
 }
--- components/DeckList.js
+++ components/DeckList.js
@@ -1,23 +1,39 @@
-import React, { Component } from "react";
-import { View, Text } from "react-native";
+import React, { useState } from "react";
+import { Text, View, ActivityIndicator, TouchableOpacity, Alert } from "react-native";
 import { getDeck } from "../utils/api";
+import { useFocusEffect } from "@react-navigation/native";
 
-export default class DeckList extends Component {
-	state = {
-		data: "",
-	};
 
-	componentDidMount() {
-		getDeck().then((data) => {
-			this.setState({ data: data });
-		});
-	}
+export default function DeckList({ route, navigation }) {
+	const [deck, setDeck] = useState({});
+	const [isLoading, setIsLoading] = useState(true);
+	const { title } = route.params;
 
-	render() {
+	useFocusEffect(() => {
+		getDeck(title)
+			.then((data) => {
+				setDeck(data);
+			})
+			.then(() => setIsLoading(false))
+			.catch((err) => Alert.alert(err));
+	}, [title]);
+
+	if (isLoading) {
 		return (
 			<View>
-				<Text>Deck List</Text>
+				<ActivityIndicator size="large" />
 			</View>
 		);
 	}
+	return (
+		<View style={styles.container}>
+			<Text style={[styles.deckTitle, { margin: 50 }]}>Number of cards: {deck.questions.length}</Text>
+			<TouchableOpacity style={styles.button} onPress={() => navigation.navigate("NewQuestion", { title: deck.title })}>
+				<Text>Add Card</Text>
+			</TouchableOpacity>
+			<TouchableOpacity style={styles.button} onPress={() => navigation.navigate("QuizView", { title: deck.title })}>
+				<Text>Start a Quiz</Text>
+			</TouchableOpacity>
+		</View>
+	);
 }
